cf0525c639ddd00ba4111ba1d1c06ab7ef3e0e6f
Ilhom5355 <ilhom575355@gmail.com>
2021-11-16 09:32:08 +0500

    Share mydemo4

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  static final String id = "home_page";
+
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MyDemo"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
